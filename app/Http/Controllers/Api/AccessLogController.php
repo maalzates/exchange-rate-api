0a4b7c2527a063dbad8c294e9051099fac5ecc70
Manuel Alzate <maalzates@gmail.com>
2024-03-20 12:38:37 -0500

    feat(access-logs): AccessLogController - Query params start_date, end_date for filtered results

--- app/Http/Controllers/Api/AccessLogController.php
+++ app/Http/Controllers/Api/AccessLogController.php
@@ -8,10 +8,21 @@ use Illuminate\Http\Request;
 
 class AccessLogController extends Controller
 {
-    public function index() {
-        
-        $access_logs = AccessLog::all();
+    public function index(Request $request) {
 
-        return response()->json($access_logs);
+        // Retrieve query parameters
+        $startDate = $request->query('start_date');
+        $endDate = $request->query('end_date');
+
+        // Query builder, will gather results if startDate or endDate params come in the query. 
+        $accessLogs = AccessLog::when($startDate, function ($query) use ($startDate) {
+            return $query->whereDate('access_time', '>=', $startDate);
+        })
+        ->when($endDate, function ($query) use ($endDate) {
+            return $query->whereDate('access_time', '<=', $endDate);
+        })
+        ->get();
+
+        return response()->json($accessLogs);
     }
 }
