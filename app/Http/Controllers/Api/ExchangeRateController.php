c0caf23f3b6eecbc04cee08afea3db068d966778
Manuel Alzate <maalzates@gmail.com>
2024-03-21 10:00:36 -0500

    feat(exchange-controller): Implementing error handling in exchangeRate controller

--- app/Http/Controllers/Api/ExchangeRateController.php
+++ app/Http/Controllers/Api/ExchangeRateController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Services\ExchangeRateService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ExchangeRateController extends Controller
 {
@@ -16,8 +17,17 @@ class ExchangeRateController extends Controller
     }
 
     public function index() {
-        $rates =  $this->exchangeRateService->getRates();
 
-        return response()->json($rates);
+        try {
+            $rates =  $this->exchangeRateService->getRates();
+
+            return response()->json($rates);
+
+        } catch (\Throwable $e) {
+            Log::error('ExchangeRateService error: ' . $e->getMessage());
+
+            return response()->json(['error' => 'An unexpected error occurred while fetching exchange rates.'], 500);
+        }
+
     }
 }
